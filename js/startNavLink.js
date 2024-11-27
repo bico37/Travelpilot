d5220f8fd14ac4b772c478e538e447629fbc79b7
Erik Bergmair <e.bergmair@HTBLALeonding.onmicrosoft.com>
2024-11-27 10:18:24 +0100

    fixed paths for subpage for hosting

---
+++ js/startNavLink.js
@@ -0,0 +1,73 @@
+let currentPage;
+function setCurrentPage() {
+  currentPage = window.location.pathname.split("/").pop();
+
+  localStorage.setItem("currentPage", currentPage);
+}
+setCurrentPage();
+
+
+// ---------------- Start ---------------------
+document.getElementById("startFree").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/yourTrip-2nd.html";
+});
+
+// NAV
+document.getElementById("backToStart").addEventListener("click", function () {
+    // Link zur Startseite
+    window.location.href = "./index.html"; // bleibt gleich
+});
+document
+    .getElementById("yourTripText")
+    .addEventListener("click", function () {
+        // Link zu yourTrip-2nd im html-Ordner
+        window.location.href = "./html/yourTrip-2nd.html"; // geändert
+    });
+document.getElementById("hotelsText").addEventListener("click", function () {
+    // Link zu hotel im html-Ordner
+    window.location.href = "./html/hotel.html"; // geändert
+});
+document.getElementById("flightsText").addEventListener("click", function () {
+    // Link zu flights im html-Ordner
+    window.location.href = "./html/flights.html"; // geändert
+});
+document.getElementById("carsText").addEventListener("click", function () {
+    // Link zu cars im html-Ordner
+    window.location.href = "./html/cars.html"; // geändert
+});
+document
+    .getElementById("activitiesText")
+    .addEventListener("click", function () {
+        // Link zu activities im html-Ordner
+        window.location.href = "./html/activities.html"; // geändert
+    });
+
+//Back To Top
+let mybutton = document.getElementById("scrollToTop");
+mybutton.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+//Footer Links
+document.getElementById("FyourTrip").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/yourTrip-2nd.html";
+});
+document.getElementById("Fhotels").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/hotel.html";
+});
+document.getElementById("Fflights").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/flights.html";
+});
+document.getElementById("Fcars").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/cars.html";
+});
+document.getElementById("Factivities").addEventListener("click", function () {
+    // Hier kommt der Code, der ausgeführt werden soll, wenn das Element geklickt wird
+    window.location.href = "./html/activities.html";
+});
+
